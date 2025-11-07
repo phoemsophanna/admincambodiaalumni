febfd21d517e287f17d32773a12ba0ecd206f3c1
phoemsophanna <phannaphoem@gmail.com>
2025-11-07 21:45:51 +0700

    last_update_admin

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
 	api: {
 		API_URL: "https://api-node.themesbrand.website",
 		//https://api.camgotech.acitytech.com
-		BASE_URL: "https://apicaa.acitycloud.com/api",
-		FILE_URI: "https://apicaa.acitycloud.com/uploads",
+		BASE_URL: "https://api.cambodiaalumni.org/api",
+		FILE_URI: "https://api.cambodiaalumni.org/uploads",
 		// BASE_URL: "https://apicda.cinvoice.com/api",
 		// RESOURCE: "https://apicda.cinvoice.com/uploads",
 		WEBSITE: "https://camgotech.acitytech.com",
